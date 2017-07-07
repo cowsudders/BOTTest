b6600ab32e2caf8766a5be65331cc53635de1ec0
cowsudders <david.p.wright@accenture.com>
2017-07-07 13:17:36 +0100

    Add files via upload

---
+++ UniversalBot.js
@@ -0,0 +1,36 @@
+let builder = require("botbuilder");
+let restifyHere = require("restify");
+
+const connector = new builder.ChatConnector();
+
+// Create BOT
+var bot = new builder.UniversalBot(connector);
+
+// Setup Restify Server
+const server = restifyHere.createServer();
+server.post('/api/messages', connector.listen());
+
+server.listen(3978, '::', () => {
+    console.log('server up');
+    console.log('This is it');
+
+})
+
+// Dialog handling
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/askName');
+    },
+    function (session, results) {
+        session.send('Hello %s', results.response);
+    }
+]);
+
+bot.dialog('/askName', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
